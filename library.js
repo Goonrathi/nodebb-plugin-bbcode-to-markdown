776a012d2b8b19655cb0bc1bcb40bae33a4299a0
Goonrathi <37307254+Goonrathi@users.noreply.github.com>
2019-01-18 12:30:54 -0800

    Update library.js

--- library.js
+++ library.js
@@ -13,9 +13,9 @@ function parseQuotes(content) {
 		quoteBlock = quote.replace(re, '$2').replace(/[\r\n]/g, '\n>')
 
 		// SomethingAwful Customizations
-		.replace(/\[video.+?([\d]*).\]([\[a-zA-Z0-9]*)\[\/video\]/gi,'\n>[youtube link](https:youtube.com/watch=$2&t=$1)')
+		// (none yet)
 		
-		//finalize block
+		// finalize block
 		content = content.replace(quote, quoteBlock);
 	}
 
@@ -25,18 +25,23 @@ function parseQuotes(content) {
 converter.parse = function(postContent) {
 	postContent = postContent
 		.replace('&#58;', ':')
-		.replace(/\[\S?color[\s\S]*?\]/gi, '')
-		.replace(/\[\S?b[s\S]*?\]/gi, '**')
-		.replace(/\[url=(https?:[\s\S]*?)\]([\s\S]*?)\[\/url\]/gi, '[$2]($1)')
-		.replace(/\[\S?url[s\S]*?\]/gi, '')
-		.replace(/\[\S?i[s\S]*?\]/gi, '*')
+		.replace(/\[\S?color[\s\S]*?\]/gi, '') //colors are removed entirely
+		.replace(/\[\S?b[s\S]*?\]/gi, '**') //bolds
+		.replace(/\[url=(https?:[\s\S]*?)\]([\s\S]*?)\[\/url\]/gi, '[$2]($1)') //urls
+		.replace(/\[\S?url[s\S]*?\]/gi, '') //not sure what this is doing
+		.replace(/\[\S?i[s\S]*?\]/gi, '*') //italics
 		.replace(/\[quote:[\s\S]*?\]([\s\S]*?)\[\/quote:[\s\S]*?\]/gi, '> $1')
-		.replace(/<!--[\s\S]*?href="([\s\S]*?)">([\s\S]*?)<[\s\S]*?-->/gi, '[$2]($1)')
+		.replace(/<!--[\s\S]*?href="([\s\S]*?)">([\s\S]*?)<[\s\S]*?-->/gi, '[$2]($1)') //urls
+
 
 		// SomethingAwful Customizations
-		.replace(/(@.+)/gi,'')
+		.replace(/\[video.+?([\d]*).\]([\[a-zA-Z0-9]*)\[\/video\]/gi,'[youtube link](https:youtube.com/watch=$2&t=$1)')
+		.replace(/\[list\]/,'<ul>') //lists open
+		.replace(/\[\*\]([\s\w]+)/,'<li>$1</li>') //lists open
+		.replace(/\[\/list\]/,'</ul>') //lists close
+		.replace(/./,'')
 		//emote customization
-		.replace(/\:(.+?)\:/gi,'<img src="emotes\\$1.gif" alt="$1 emote">')
+		.replace(/\:(.+?)\:/gi,'<img src="/images/emotes/$1.gif" alt="$1 emote">')
 
 	postContent = parseQuotes(postContent);
 	return postContent
