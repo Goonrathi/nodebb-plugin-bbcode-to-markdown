81e0c4da68a7081c34cde3b59c1e0f4fb324e24b
Goonrathi <37307254+Goonrathi@users.noreply.github.com>
2019-01-18 15:15:25 -0800

    Update library.js

--- library.js
+++ library.js
@@ -6,7 +6,7 @@ var converter = {};
 
 function parseQuotes(content) {
 	var quote, quoteBlock,
-		re = /\[quote=["]?([\s\S]*?)["]?\]([\s\S]*?)\[\/quote\]/gi;
+		re = /\[quote=?["]?([\s\S]*?)["]?\]([\s\S]*?)\[\/quote\]/gi;
 
 	while(quote = content.match(re)) {
 		quote = quote[0];
@@ -29,17 +29,18 @@ converter.parse = function(postContent) {
 		.replace(/\[\S?b[s\S]*?\]/gi, '**') //bolds
 		.replace(/\[url=(https?:[\s\S]*?)\]([\s\S]*?)\[\/url\]/gi, '[$2]($1)') //urls
 		.replace(/\[\S?url[s\S]*?\]/gi, '') //not sure what this is doing
-		.replace(/\[\S?i[s\S]*?\]/gi, '*') //italics
-		.replace(/\[quote:[\s\S]*?\]([\s\S]*?)\[\/quote:[\s\S]*?\]/gi, '> $1')
+		.replace(/\[\/?i\]/gi, '*') //italics
+		// .replace(/\[quote:?[\s\S]*?\]([\s\S]*?)\[\/quote:[\s\S]*?\]/gi, '> $1')
 		.replace(/<!--[\s\S]*?href="([\s\S]*?)">([\s\S]*?)<[\s\S]*?-->/gi, '[$2]($1)') //urls
 
 
 		// SomethingAwful Customizations
 		.replace(/\[video.+?([\d]*).\]([\[a-zA-Z0-9]*)\[\/video\]/gi,'[youtube link](https:youtube.com/watch=$2&t=$1)')
+		.replace(/\[t*img\](.+?)\[\/t*img\]/gi,'<img src="/images/posts/$1>') //img and timg
 		.replace(/\[list\]/,'<ul>') //lists open
 		.replace(/\[\*\]([\s\w]+)/,'<li>$1</li>') //lists open
 		.replace(/\[\/list\]/,'</ul>') //lists close
-		.replace(/./,'')
+		// .replace(/./,'')
 		//emote customization
 		.replace(/\:(.+?)\:/gi,'<img src="/images/emotes/$1.gif" alt="$1 emote">')
 
